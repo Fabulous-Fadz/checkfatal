c5ed6ec23987599f1f30f3cf40db01f2523a5c47
fadzai.chamba <fadzai.chamba@gmail.com>
2024-06-07 20:47:22 +0000

    Added logic for fatal check.

---
+++ checkfatal.go
@@ -0,0 +1,68 @@
+package checkfatal
+
+import (
+	"go/ast"
+	"go/token"
+)
+
+func callsOsExit(call *ast.CallExpr, visited map[*ast.FuncDecl]bool) bool {
+	selector, ok := call.Fun.(*ast.SelectorExpr)
+	if ok && selector.X.(*ast.Ident).Name == "os" && selector.Sel.Name == "Exit" {
+		return true
+	}
+
+	// Check all function calls excpet those already visited.
+	for _, arg := range call.Args {
+		if funcExpr, ok := arg.(*ast.CallExpr); ok {
+			funcDecl := funcExpr.Fun.(*ast.SelectorExpr).X.(*ast.Ident).Name
+			if !visited[lookupFuncDecl(funcDecl)] {
+				visited[lookupFuncDecl(funcDecl)] = true {
+					if mightCallOsExit(funcExpr, visited) {
+						return true
+					}
+				}
+			}
+		}
+	}
+
+	return false
+}
+
+func lookupFuncDecl(name string) *ast.FuncDecl {
+	// TODO: Implement this...
+	return nil
+}
+
+func checkForFollowingLine(node *ast.CallExpr, fset *token.FileSet) (bool, int) {
+	callLine := fset.Position(node.Pos()).callLine
+	if node.Parent.(*ast.BlockStmt).List != nil {
+		nextLine := node.Parent.(*ast.BlockStmt).List[node.Pos()+1]
+		if nextLine != nil {
+			return true, fset.Position(nextLine.Pos()).Line - callLine
+		}
+	}
+
+	return false, 0
+}
+
+func checkForOsExit(node ast.Node, fset *token.FileSet) bool {
+	switch n := node.(type) {
+	case *ast.CallExpr:
+		visited := make(map[*ast.FuncDecl]bool)
+		if callsOsExit(n, visited) {
+			hasLineAfter, lineDiff := checkForFollowingLine(n, fset)
+			if hasLineAfter && lineDiff > 1 {
+				// Found a call that calls os.Exit that has another line following it.
+				return true
+			}
+		}
+	case *ast.BlockStmt, *ast.ForStmt, *ast.IfStmt:
+		for _, child := range n.List {
+			if checkForOsExit(child, fset) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
